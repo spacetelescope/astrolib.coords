6b85e38c64778c005b239081f6fa57cf9164e473
laidler <laidler@stsci.edu>
2006-05-01 22:33:23 +0000

    Updated documentation.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/coords@53 90a0a646-be8a-0410-bb88-9290da87bc01

--- lib/__init__.py
+++ lib/__init__.py
@@ -25,31 +25,55 @@ Example
 =======
 
 >>> import coords as C
->>>
->>> #Specify position in hmsdms
->>> polaris=C.Position("02:31:49.08 +89:15:50.8")
->>> polaris.dd()
-(37.954500000000003, 89.264111111111106)
->>> polaris.hmsdms()
-"02:31:49.080 +89:15:50.800"
->>> #Specify position in decimal degrees
->>> ob=C.Position((52.9860209, -27.7510006))
+#
+#Unit conversions
+>>> ob=C.Position('12:34:45.34 -23:42:32.6')
 >>> ob.hmsdms()
-"03:31:56.645 -27:45:03.602"
+'12:34:45.340 -23:42:32.600'
 >>> ob.dd()
-(52.9860209, -27.751000600000001)
->>>
->>> #Use as calculator without saving the intermediate object
->>> C.Position("12:34:45.4 -22:21:45.4").dd()
-(188.68916666666667, -22.362611111111111) 
+(188.68891666666667, -23.709055555555555)
+>>> ob.rad()
+(3.2932428578545374, -0.41380108198269777)
+#
+#Angular separations
+>>> p1=C.Position("01:23:45.300 +65:43:31.240")
+>>> p2=C.Position("01:23:45.62 +65:43:31.20")
+>>> p1.angsep(p2)
+0.000548 degrees
+>>> delta=p1.angsep(p2)
+>>> delta.arcsec()
+1.973739377865491
+>>> p1.within(p2,3.0,units='arcsec')
+True
+#
+# Coordinate conversions
+>>> ob.j2000()
+(188.68891666666667, -23.709055555555555)
+>>> ob.b1950()
+(-171.96943519057595, -23.433637283819877)
+>>> ob.galactic()
+(-61.983610612512024, 39.003358150874568)
+>>> ob.ecliptic()
+(-162.41539533987057, -18.294212209103392)
+
+
+
+For more examples, see the wiki page listed below.
+
+TPM Citation
+============
+Investigators using this software for their research are requested to
+explicitly acknowledge "use of the TPM software library, by Jeffrey W.
+Percival" in any appropriate publications. 
 
 @sort: Position, Coord, Degrees, Hmsdms
 
 
-@see: U{http://www.scipy.org/wikis/topical_software/AstroLibCoordsHome}
+@see: U{http://www.scipy.org/AstroLibCoordsHome}
 
 @author:  Vicki Laidler
 @version: '0.0.2 (2006-1-30)'
+
 
 
 @group Classes: position.Position, position.Coord, angsep.Angsep
