72f8857a7d83a894dd823ea9036e2fe0479b78b1
laidler <laidler@stsci.edu>
2006-05-09 21:21:45 +0000

    Make this test suite run silently if successful, just like all the others.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/coords@68 90a0a646-be8a-0410-bb88-9290da87bc01

--- lib/testpos.py
+++ lib/testpos.py
@@ -30,31 +30,31 @@ def test_zero_hms(t):
     p=P.Position(t.hms_zero)
     res=p.dd()
     assert res == t.dd_zero, "Fail: result = " + `res`
-    print "test_zero_hms: pass"
+#    print "test_zero_hms: pass"
     
 def test_zero_dd(t):   
     p=P.Position(t.dd_zero)
     res=p.hmsdms()
     assert res == t.hms_zero, "Fail: result = " + res
-    print "test_zero_dd: pass"
+#    print "test_zero_dd: pass"
 
 def test_zero_rad(t):
     p=P.Position(t.rad_zero,units='rad')
     res=p.dd()
     assert res == t.dd_zero, "Fail: result = (%f,%f)"%(res)
-    print "test_zero_rad: pass"
+#    print "test_zero_rad: pass"
     
 def test_posarb_hms(t):
     p=P.Position(t.hms_posarb)
     res=p.dd()
     assert fuzzy_tuple(res,t.dd_posarb,t.epsilon), "Fail: result = " + `res`
-    print "test_posarb_hms: pass"
+#    print "test_posarb_hms: pass"
 
 def test_posarb_dd(t):
     p=P.Position(t.dd_posarb)
     res=p.hmsdms()
     assert res == t.hms_posarb, "Fail: result = "+res
-    print "test_posarb_dd: pass"
+#    print "test_posarb_dd: pass"
 
 def test_posarb_rad(t):
     p=P.Position(t.rad_posarb,units='rad')
@@ -65,13 +65,13 @@ def test_negarb_hms(t):
     p=P.Position(t.hms_negarb)
     res=p.dd()
     assert fuzzy_tuple(res,t.dd_negarb,t.epsilon), "Fail: result = " + `res`
-    print "test_negarb_hms: pass"
+#    print "test_negarb_hms: pass"
 
 def test_negarb_dd(t):
     p=P.Position(t.dd_negarb)
     res=p.hmsdms()
     assert res == t.hms_negarb, "Fail: result = "+res
-    print "test_negarb_dd: pass"
+#    print "test_negarb_dd: pass"
 
 def test_negarb_rad(t):
     p=P.Position(t.rad_negarb,units='rad')
@@ -82,20 +82,20 @@ def test_details(t):
     p=P.Position(t.dd_posarb)
     res=p.details()
     assert res == t.detail_string, "Fail: result = \n"+res
-    print "test_details: pass"
+#    print "test_details: pass"
 
 def test_angsep1(t):
     p1=P.Position(t.dd_posarb)
     sep=p1.angsep(p1)
     assert sep._internal == 0.0 , "Fail angsep1: result=%f"%sep._internal
-    print "test_angsep1: pass"
+#    print "test_angsep1: pass"
 
 def test_angsep2(t):
     p1=P.Position((t.dd_posarb[0],0.0))
     p2=P.Position((t.dd_posarb[0]+2.5,0.0))
     sep=p1.angsep(p2)
     assert fuzzy_equal(sep._internal,2.5,0.0000001), "Fail angsep2: abs(sep-2.5) > 0.0000001)"
-    print "test_angsep2: pass"
+#    print "test_angsep2: pass"
 
 def test_angsep3(t):
     """This test answer taken from IDL gcirc routine"""
@@ -104,27 +104,27 @@ def test_angsep3(t):
     sep=p1.angsep(p2)
     sep.setunits('rad')
     assert fuzzy_equal(sep.value,2.15489990,0.0000001), "Fail angsep3: %f, %f"%(sep.value,2.15489990)
-    print "test_angsep3: pass"
+#    print "test_angsep3: pass"
 
 def test_within1(t):
     p1=P.Position(t.dd_posarb)
     p2=P.Position((t.dd_posarb[0],t.dd_posarb[1]+0.24))
     ans=p1.within(p2,0.3,units='degrees')
     assert ans, "Fail within1"
-    print "test_within1: pass"
+#    print "test_within1: pass"
 
 def test_within2(t):
     p1=P.Position(t.dd_posarb)
     p2=P.Position((t.dd_posarb[0],t.dd_posarb[1]+0.24))
     ans=p1.within(p2,0.3,units='arcsec')
     assert not ans, "Fail within2"
-    print "test_within2: pass"
+#    print "test_within2: pass"
 
 def test_within3(t):
     p1=P.Position(t.dd_posarb)
     ans=p1.within(p1,0.2)
     assert ans, "Fail within3"
-    print "test_within3: pass"
+#    print "test_within3: pass"
               
 def run():
     t=Tvalues()
