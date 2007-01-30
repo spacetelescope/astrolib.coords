b8e67240378ecc82a35d31c69219f2fc1efdb2e3
laidler <laidler@stsci.edu>
2007-01-30 20:25:04 +0000

    pytpm_wrapper.blackbox: accept a timetag argument, but default to utcnow.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/coords@141 90a0a646-be8a-0410-bb88-9290da87bc01

--- lib/pytpm_wrapper.py
+++ lib/pytpm_wrapper.py
@@ -8,10 +8,11 @@ have been made there; but the modulo operator in C only works on integers,
 so it was simpler to do it in python. Also, this leaves pytpm itself as a more
 transparent wrapper for TPM."""
 
-def blackbox(x,y,instate,outstate,epoch,equinox):
-    #Temporarily pass in utc now as the timetag:
-    timetag=astrodate.AstroDate().jd
-    r,d=pytpm.blackbox(x,y,instate,outstate,epoch,equinox,timetag)
+def blackbox(x,y,instate,outstate,epoch,equinox,timetag=None):
+    if timetag == None:
+        timetag=astrodate.AstroDate()
+
+    r,d=pytpm.blackbox(x,y,instate,outstate,epoch,equinox,timetag.jd)
     #Convert longitude to astrolib/coords convention
     r=r%360.0
     return r,d
