46898da4b2e9d17b6a2c0f271e369ba6e739fb5f
laidler <laidler@stsci.edu>
2007-02-12 18:10:38 +0000

    Modified to reflect refactoring of test modules.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/coords@146 90a0a646-be8a-0410-bb88-9290da87bc01

--- lib/test_all.py
+++ lib/test_all.py
@@ -1,8 +1,11 @@
-"""Convenience module to run all unit tests."""
+"""Developer-use convenience module to run all unit tests."""
+
 import testpos, test_angsep, test_tpm, test_astrodate, test_snapshot
 def run():
+    import coords
+    coords._test() #snapshot test
     testpos.run()
     test_angsep.run()
     test_tpm.run()
     test_astrodate.run()
-    test_snapshot.run()
+
