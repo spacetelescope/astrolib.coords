d10aeacc680cf48892af6d4a7d57d876ae91ba1d
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-24 16:57:14 -0400

    Converted to setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python
+import os
+import fnmatch
+from setuptools import setup, find_packages, Extension
+from version import get_git_version
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+SOURCES = [
+    os.path.join(root, f)
+    for root, _, files in os.walk('src')
+    for f in files if f.endswith('.c')
+]
+
+git_version = get_git_version()
+with open('lib/astrolib/coords/version.py', 'w') as version_data:
+    version_data.write('__version__ = {0}'.format(git_version))
 
 setup(
-    setup_requires=['d2to1>=0.2.3', 'stsci.distutils'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name = 'astrolib.coords',
+    version = git_version,
+    author = 'Vicki Laidler',
+    author_email = 'help@stsci.edu',
+    description = 'Astronomical coordinates & angular separations (OO)',
+    url = 'http://www.astrolib.org',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'nose',
+        'numpy',
+    ],
+
+    package_dir = {
+        '':'lib',
+    },
+    packages = find_packages('lib/*'),
+    ext_modules = [
+        Extension('astrolib.coords._pytpm', sources=SOURCES)
+    ]
 )
